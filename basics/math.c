bbe88f4d96278b191a93dc2666c607dbaa61bd56
Soner <sonersg@outlook.com>
2024-10-30 19:33:18 +0300

    math

---
+++ basics/math.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    double A = sqrt(9);
+    double B = pow(2, 6);
+    int C = round(3.14);
+    int D = ceil(3.009);
+    int E = floor(3.99);
+    double F = fabs(-21);
+    double G = log(3);
+    double H = sin(45);
+    double I = cos(45);
+    double J = tan(45);
+
+    printf("%d", D);
+    // printf("%f", H);
+
+    return 0;
+}
--- basics/user_input.c
+++ basics/user_input.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
 
     int age;
+    char name[25];
 
-    printf("\nHow old are you? \n");
+    // printf("\nHow old are you? \n");
+    // scanf("%d", &age);
+    // printf("You are %d years old.", age);
 
-    scanf("%d", &age);
-
-    printf("You are %d years old.", age);
+    printf("\nWhat is your name? \n");
+    fgets(name, 25, stdin);
+    // name[strlen(name) - 1] = '\0';
+    printf("Your name is %s ", name);
 
     return 0;
 }
