292e85d7b642c561ebbc1261fc92f2c6aa2cf189
sonersg <sonersg@outlook.com>
2025-08-08 15:52:48 +0300

    vs-shortcuts

---
+++ basics/vs_shortcuts.c
@@ -0,0 +1,32 @@
+
+
+// VS CODE SHORTCUTS
+
+// ctrl is command in macos
+
+// if there are 2 keys, first 1 press and hold
+// if there are 3 keys, first 2 press and hold
+// n - 1, press and hold
+
+////////////////////////////////////////////
+
+// alt + shift + up/down
+// alt + up/down
+// alt + click
+// ctrl + ö
+// ctrl + enter
+// ctrl + alt + n
+// ctrl + shift + u
+
+//////////////////////////////////////////
+
+#include <stdio.h>
+
+int main()
+{
+
+    int age = 44;
+    printf("%d", age);
+
+    return 0;
+}
